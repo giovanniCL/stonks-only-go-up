99bd514d8126515d8221156715d3db89c59c084e
Alan Chu <alanchu394@gmail.com>
2021-03-15 03:32:17 -0400

    login page

---
+++ stonks-only-go-up/src/Components/Login/LogInPage.js
@@ -0,0 +1,25 @@
+// General Imports
+import { React } from "react"
+import "../Login/login.css"
+// Log In Page
+/*
+    Page is the first step of the process in logging into account. This will appear
+    right after the user clicks the log in button (username, password)
+*/
+// <button onClick={() => props.history.push('/setup/personal-info')}>Begin Account Setup</button>
+const LogInPage = (props) => {
+    return (
+
+        <div class = "loginBox">
+            <h1>Log In</h1>
+
+            <div class = "row">
+                <input type="text" name="username" placeholder="Username" required></input>
+                <input type="password" name="password" placeholder="Password" required></input>
+                <input type="submit" value="Login"></input>
+                <button>Forget Password</button>
+            </div>
+        </div>
+    )
+}
+export default LogInPage
