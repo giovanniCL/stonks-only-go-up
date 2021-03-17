df6da9134aeeb029382f355b332825d405d00883
partisi <erolmb@gmail.com>
2021-03-16 20:05:35 -0400

    setup stonks interests

--- stonks-only-go-up/src/Components/Setup/SetupInterestPage.js
+++ stonks-only-go-up/src/Components/Setup/SetupInterestPage.js
@@ -1,14 +1,42 @@
 // General Imports
-import { React } from "react"
-
+import { React, useState } from "react"
+import { interestsList } from "../InterestsList"
 // Setup Interest Page
 /*
     This page is the THIRD step of the setting up account
 */
 const SetupInterestPage = (props) => {
+    const [interestsSelected, selectInterest] = useState([])
+    function interestClicked(eachInterest) {
+        let interestsSelectedWorking = [...interestsSelected]
+        let interestIndex = interestsSelectedWorking.indexOf(eachInterest)
+        if (interestIndex === -1) {
+            selectInterest([...interestsSelectedWorking, eachInterest])
+        } else {
+            interestsSelectedWorking.splice(interestIndex, 1)
+            selectInterest(interestsSelectedWorking)
+        }
+    }
     return (
         <div className="setup-page-wrapper">
             <h1 className="setup-header">Interest Page</h1>
+            <p>Select the following interests that you enjoy the most:</p>
+            <ul className="interest-list">
+                {interestsList.map((eachInterest, eachInterestIndex) => {
+                    return (
+                        <li className="each-interest-item" key={eachInterestIndex}>
+                            <button
+                                className={interestsSelected.includes(eachInterest) ?
+                                    "interest-bttn interest-selected" :
+                                    "interest-bttn"
+                                }
+                                onClick={() => interestClicked(eachInterest)}>
+                                {eachInterest}
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
             <div className="setup-directory">
                 <button className="generic-path-button" onClick={() => props.history.push('/setup/personal-info')}>Back</button>
                 <button className="generic-path-button" onClick={() => props.history.push('/setup/stonk-suggest')}>Continue</button>
