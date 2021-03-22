f042ee78f3cbfee0703972a2f1f0e4411870ed9e
ad4565 <54941861+ad4565@users.noreply.github.com>
2021-03-22 15:28:14 -0400

    Update SingleStonk.js

--- stonks-only-go-up/src/Components/SingleStonk.js
+++ stonks-only-go-up/src/Components/SingleStonk.js
@@ -1,12 +1,8 @@
 import React from 'react'
-//This is just to test that you can click on the stonk preview to get to a single stonk page,
-//whoever does this page feel free to use this file or just delete it and create a new one
 
-const SingleStonk = ({ match }) => {
+
+function singleStonk(){
     return(
-        <h1> You clicked on {match.params.name}</h1>
+        <div></div>
     )
 }
-
-
-export default SingleStonk
