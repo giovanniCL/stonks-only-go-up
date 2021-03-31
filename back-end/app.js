09ee4f7f5c895b7f456ace8b1683d869c0493461
unknown <giovanniciccia1@gmail.com>
2021-03-31 14:47:58 -0400

    added hype endpoint, also installed axios, cors and nodemon dependencies

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,17 @@
+const axios = require('axios')
 const express = require("express")
+const cors = require('cors')
 const app = express()
 
 app.use(express.static('public'))
 
 //ROUTES GO HERE
 app.get('/', (req,res)=>{
+})
+
+app.get('/hype', cors(), async (req,res) => {
+    let response = await axios("https://my.api.mockaroo.com/stonks.json?key=7d2830f0")
+    res.json(response.data)
 })
 //
 module.exports = app
