4fd713d3df379b0b62d3a0477658ad7225c37e29
unknown <giovanniciccia1@gmail.com>
2021-04-01 20:55:54 -0400

    added followed endpoint

--- back-end/app.js
+++ back-end/app.js
@@ -4,12 +4,18 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.static('public'))
+app.use(cors())
 
 //ROUTES GO HERE
 app.get('/', (req,res)=>{
 })
 
-app.get('/hype', cors(), async (req,res) => {
+app.get('/hype', async (req,res) => {
+    let response = await axios("https://my.api.mockaroo.com/stonks.json?key=7d2830f0")
+    res.json(response.data)
+})
+
+app.get('/followed', async (req,res) => {
     let response = await axios("https://my.api.mockaroo.com/stonks.json?key=7d2830f0")
     res.json(response.data)
 })
