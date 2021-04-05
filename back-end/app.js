88486054a7237c6d685758080bd09a36d87786e9
Alan Chu <alanchu394@gmail.com>
2021-04-04 21:15:18 -0400

    login/reset express routes

--- back-end/app.js
+++ back-end/app.js
@@ -8,7 +8,15 @@ app.use(express.static('public'))
 //ROUTES GO HERE
 app.get('/', (req,res)=>{
 })
+app.get('/login', (req, res) => {
+    //Nothing here yet
+    //waiting for account database
+})
 
+app.get('/reset', (req, res) => {
+    //Nothing here yet
+    //waiting for account database
+})
 app.get('/hype', cors(), async (req,res) => {
     let response = await axios("https://my.api.mockaroo.com/stonks.json?key=7d2830f0")
     res.json(response.data)
