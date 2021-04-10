95f933c8c5aeae42c5634e1325212abeaab2a4e0
unknown <giovanniciccia1@gmail.com>
2021-04-10 12:46:13 -0400

    created stonk and user schemas

---
+++ back-end/schemas.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+const User= mongoose.model('User',{
+    firstname:{
+        type: String,
+        required:true
+    },
+    lastname:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+const Stonk = mongoose.model('Stonk',{
+    name:{
+        type: String
+    },
+    symbol:{
+        type: String,
+        required: true,
+    },
+    stonkometer:{
+        type: Number,
+    },
+    openPrice:{
+        type: Number,
+        required: true
+    },
+    highPrice:{
+        type: Number,
+        required: true
+    },
+    lowPrice:{
+        type: Number,
+        required: true
+    },
+    currentPrice:{
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = {User, Stonk}
