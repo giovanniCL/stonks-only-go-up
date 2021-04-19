c49c4276ddeeead26b1d635eee33ab90f4dfa39e
Alan Chu <alanchu394@gmail.com>
2021-04-18 22:27:35 -0400

    sign up form sends data to mongo

--- stonks-only-go-up/src/Components/Signup/SignUp.js
+++ stonks-only-go-up/src/Components/Signup/SignUp.js
@@ -3,6 +3,10 @@ import "./signup.css"
 import Navbar from '../Navbar'
 
 const SignUp = (props) => {
+    let handleLogin = (event) => {
+        event.preventDefault();
+        props.history.push('/setup/initial')
+      }
     return(
         <>
         <div> <Navbar /> </div>
@@ -19,10 +23,12 @@ const SignUp = (props) => {
             <input type = "text" name ="userName" placeholder = "User Name" required></input>
             <input type = "text" name ="email" placeholder = "Email" required></input>
 
-            <input type = "text" className = "password" name ="password" placeholder = "Password" required></input>
-            <input type = "text" className= "password" name ="confirmPassword" placeholder = "asdad Password" required></input>
+            <input 
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+            type = "text" className = "password" name ="password" placeholder = "Password" required></input>
+            <input type = "text" className= "password" name ="confirmPassword" placeholder = "Confirm Password" required></input>
             
-            <button type = "submit" >Create Account</button>
+            <button onSubmit={(event) => {event.preventDefault();props.history.push('/setup/initial');}} >Create Account</button>
             {/* onClick={() => props.history.push('/setup/initial')} */}
          </form>
       </div>
