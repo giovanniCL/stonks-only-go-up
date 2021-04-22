1db8c875b45fc072fb8d6d357e996f95d40ae2a8
Alan Chu <alanchu394@gmail.com>
2021-04-21 21:17:14 -0400

    requires valid email

--- stonks-only-go-up/src/Components/Signup/SignUp.js
+++ stonks-only-go-up/src/Components/Signup/SignUp.js
@@ -17,12 +17,13 @@ const SignUp = (props) => {
             <input type = "text" name ="firstName" placeholder = "First Name" required></input>
             <input type = "text" name ="lastName" placeholder = "Last Name" required></input>
             <input type = "text" name ="userName" placeholder = "User Name" required></input>
-            <input type = "text" name ="email" placeholder = "Email" required></input>
+            <input type = "email" name ="email" placeholder = "Email" required></input>
 
             <input 
             pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
             type = "password" className = "password" name ="password" placeholder = "Password" required></input>
             <input type = "password" className= "password" name ="confirmPassword" placeholder = "Confirm Password" required></input>
+            
             <button onSubmit={(event) => {event.preventDefault();}} >Create Account</button>
          </form>
       </div>
