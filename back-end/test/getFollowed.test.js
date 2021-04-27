d1c3d16b897e29a260bf62349a04d40000e0b8df
unknown <giovanniciccia1@gmail.com>
2021-04-27 18:29:54 -0400

    updated tests for follow and hype endpoints

--- back-end/test/getFollowed.test.js
+++ back-end/test/getFollowed.test.js
@@ -6,15 +6,33 @@ const app = require("../app.js")
 
 chai.use(chaiHTTP)
 
-describe("testing /followed/:user GET request", () => {
-    it("should return 200 ok status", () =>{
-        return chai.request(app).get('/followed/some-user').then(response =>{
-            expect(response.status).to.equal(200)
+describe("testing /follow/stonks user GET request and follow/:stonk GET request", () => {
+    it("should return an array of stonk objects in which the first object has a \"symbol\" propoerty of value \"GME\"", () =>{
+        chai.request(app).post("/api/auth/login").send({
+            user_name : "Stonk_Guy_420", password: "PASSWORD"
+        }).then(auth_response =>{
+            let auth = JSON.parse(auth_response.text).token
+            return chai.request(app).get('/follow/stonks').set("x-access-token", auth).then(response =>{
+                expect(response.status).to.equal(200)
+    
+                let response_object = JSON.parse(response.text)
+                expect(response_object).to.be.a('array')
+                expect(response_object[0]).to.be.a('object')
+                expect(response_object[0].symbol).to.equal('GME')
+    
+            })
         })
     })
-    it("should return an array of objects where the first object has a \"user\" key with a \"some-user\" value", () =>{
-        return chai.request(app).get('/followed/some-user').then(response =>{
-            expect(JSON.parse(response.text)[0].user).to.equal("some-user")
+    it("should return \"Stonk_Guy_420 followed/unfollowed SBUX\"", () => {
+        chai.request(app).post("/api/auth/login").send({
+            user_name : "Stonk_Guy_420", password: "PASSWORD"
+        }).then(auth_response =>{
+            let auth = JSON.parse(auth_response.text).token
+            return chai.request(app).get('/follow/stonks/SBUX').set("x-access-token", auth).then(response =>{
+                expect(response.status).to.equal(200)
+                expect(response.text).to.equal("Stonk_Guy_420 followed/unfollowed SBUX")
+            })
         })
     })
+ 
 })
--- back-end/test/getHype.test.js
+++ back-end/test/getHype.test.js
@@ -6,10 +6,15 @@ const app = require("../app.js")
 
 chai.use(chaiHTTP)
 
-describe("testing /hype GET request", () => {
-    it("should return 200 ok status", () =>{
-        return chai.request(app).get('/hype').then(response =>{
+describe("testing /hype/stonks GET request", () => {
+    it("should return an array of stonk objects sorted by stonkometer from highest to lowest", () =>{
+        return chai.request(app).get('/hype/stonks').then(response =>{
             expect(response.status).to.equal(200)
+
+            let response_object = JSON.parse(response.text)
+            expect(response_object).to.be.a('array')
+            expect(response_object[0]).to.be.a('object')
+            expect(response_object[0].stonkometer >= response_object[1].stonkometer).to.be.true
         })
     })
 })
