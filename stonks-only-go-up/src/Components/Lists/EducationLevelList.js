bbcb128d20821115cd5fc8538dcf0881bfd3a969
partisi <erolmb@gmail.com>
2021-05-05 01:25:11 -0400

    personal info upload select

--- stonks-only-go-up/src/Components/Lists/EducationLevelList.js
+++ stonks-only-go-up/src/Components/Lists/EducationLevelList.js
@@ -3,6 +3,8 @@
 // This page just has a list that contains the current values for the user to select
 // from within our profile setup page
 export const educationLevelList = [
+    "No Education",
+    "Homeschool",
     "Some High School",
     "Completed High School",
     "Some College",
